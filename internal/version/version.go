4deca640ceb713417a84e349179052785517f304
mmaelicke <mirko@hydrocode.de>
2025-03-19 09:26:36 +0100

    Bump version to 0.2.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version is the current version of the application
-const Version = "0.2.0"
+const Version = "0.2.1"
 
 // BuildTime is set during build time
 var BuildTime string
